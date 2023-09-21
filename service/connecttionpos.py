b49c80bfddb7dc9e30ccb3c9890e2c923755234a
John Doe <john@email.com>
2023-09-21 11:55:45 +0700

    2 commit

--- service/connecttionpos.py
+++ service/connecttionpos.py
@@ -2,11 +2,19 @@ import psycopg2
 
 
 def connectdb():
+    # connection = psycopg2.connect(
+    #     host='localhost',
+    #     user='postgres',
+    #     password='1234',
+    #     database='sampledb',
+    #     port=5432
+    # )
+
     connection = psycopg2.connect(
-        host='localhost',
-        user='postgres',
-        password='1234',
-        database='sampledb',
+        host='dpg-ck5snnj6fquc739cp7c0-a',
+        user='admin',
+        password='ivEhQfBtO0Ht35vb15CloAp9smmbz1dM',
+        database='sampledb_q4m3',
         port=5432
     )
     return connection
